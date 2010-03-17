64852da31f9ea27b3f57834ae49c396af6c56478
andrzej.chodor <andrzej.chodor@8659dc6c-7194-11de-9895-81829a6523ab>
2010-03-17 11:28:22 +0000

    @ function bug fix of Integer, Float retrieving.

--- trunk/core/src/pl/omtt/core/stdlib/Properties.java
+++ trunk/core/src/pl/omtt/core/stdlib/Properties.java
@@ -11,11 +11,11 @@ import pl.omtt.core.annotations.OmttModule;
 
 @OmttModule
 public class Properties {
-	public static Boolean isNull (Object object) {
+	public static Boolean isNull(Object object) {
 		return object == null;
 	}
-	
-	public static Boolean notNull (Object object) {
+
+	public static Boolean notNull(Object object) {
 		return object != null;
 	}
 
@@ -25,7 +25,7 @@ public class Properties {
 
 	@SuppressWarnings("unchecked")
 	public static Collection<Object> $seq(Object object, String property) {
-		return (Collection)retrieveProperty(object, property, true);
+		return (Collection) retrieveProperty(object, property, true);
 	}
 
 	@SuppressWarnings("unchecked")
@@ -34,7 +34,7 @@ public class Properties {
 		if (o instanceof Map) {
 			for (Object key : ((Map) o).keySet())
 				if (key instanceof String)
-					attrs.add((String)key);
+					attrs.add((String) key);
 		} else {
 			for (Method m : o.getClass().getMethods()) {
 				if (Object.class.equals(m.getDeclaringClass()))
@@ -42,13 +42,15 @@ public class Properties {
 				if (m.getParameterTypes().length > 0)
 					continue;
 				final int modifiers = m.getModifiers();
-				if (!Modifier.isPublic(modifiers)) 
+				if (!Modifier.isPublic(modifiers))
 					continue;
 				final String name = m.getName();
 				if (name.startsWith("get") && name.length() > 3)
-					attrs.add(name.substring(3,4).toLowerCase() + name.substring(4));
+					attrs.add(name.substring(3, 4).toLowerCase()
+							+ name.substring(4));
 				else if (name.startsWith("is") && name.length() > 4)
-					attrs.add(name.substring(2,3).toLowerCase() + name.substring(3));
+					attrs.add(name.substring(2, 3).toLowerCase()
+							+ name.substring(3));
 			}
 		}
 		return attrs;
@@ -59,7 +61,7 @@ public class Properties {
 			boolean collection) {
 		if (o == null || property == null)
 			return null;
-		
+
 		Object res;
 		if (o instanceof Map) {
 			res = ((Map) o).get(property);
@@ -67,11 +69,18 @@ public class Properties {
 			Method m = reflectPropertyMethod(o, property);
 			if (m == null)
 				res = null;
-			try {
-				res = m.invoke(o);
-			} catch (Throwable e) {
-				res = null;
-			}
+			else
+				try {
+					res = m.invoke(o);
+				} catch (Throwable e) {
+					res = null;
+				}
+		}
+		if (res != null) {
+			if (res instanceof Float)
+				res = new Double(((Float) res).doubleValue());
+			else if (res instanceof Integer)
+				res = new Long(((Integer) res).intValue());
 		}
 		if (collection) {
 			if (res instanceof Collection)
@@ -95,7 +104,7 @@ public class Properties {
 
 	private static Method reflectPropertyMethod(Object o, String name) {
 		Class<?> clazz = o.getClass();
-		String ufirst = name.substring(0,1).toUpperCase() + name.substring(1);
+		String ufirst = name.substring(0, 1).toUpperCase() + name.substring(1);
 		try {
 			return clazz.getMethod("get" + ufirst);
 		} catch (Throwable e) {
